01e2cd319893ab626ee8f97d7ab4a78a525692f1
Jack <jack.sargeant22@gmail.com>
2021-10-20 21:57:45 +0100

    commit

--- functions.php
+++ functions.php
@@ -5,33 +5,34 @@ function create_personel_data(){
     $personeldata = array();
     $json = file_get_contents("./employees-final.json");
     $personeldatapre = json_decode($json,true);
-
-    
     foreach($personeldatapre as $person){
         $elementrename =  $person["id"]."_".$person["firstname"]."_".$person["lastname"];
         $personeldata[$elementrename] = $person;
         $elementrename=""; 
     } //this adds the ID, firstname and last name to the parent array element, rather than normal.
-
     $taxdatapre = array();
     $taxdata = array();
     $json = file_get_contents("./tax-tables.json");
     $taxdatapre = json_decode($json,true);
-
-
     foreach($taxdatapre as $tax){
-        $elementrename =  "tax_band_".$tax["id"];
-        $taxdata[$elementrename] = $tax;
-        $elementrename=""; 
-    } //this creates an array from tax rate JSON, renaming the parent element to the tax band, and removing the name element
-
-    
-
+         $elementrename =  "tax_band_".$tax["id"];
+         $taxdata[$elementrename] = $tax;
+         $elementrename=""; 
+    } //this creates an array from tax rate JSON, renaming the parent element to the tax band, and removing the name elemen
+    foreach($personeldata as $key=>$person){
+        $salarytocheck = $person["salary"];     
+         if($salarytocheck <= $taxdata["tax_band_1"]["maxsalary"]){ // 10k
+             $personeldata[$key]["tax_band"] = "tax_band_1";           
+         }elseif($salarytocheck <= $taxdata["tax_band_2"]["maxsalary"]){//40k
+             $personeldata[$key]["tax_band"] = "tax_band_2";            
+         }elseif($salarytocheck <= $taxdata["tax_band_3"]["maxsalary"]){//150k
+             $personeldata[$key]["tax_band"] = "tax_band_3";          
+         }elseif($salarytocheck > $taxdata["tax_band_4"]["minsalary"]){//150k+
+             $personeldata[$key]["tax_band"] = "tax_band_4";        
+         }
+    }
+    //appends tax band to each employee, for tax calculation function.
+    return ($personeldata);
 }
-
-
-
-
-
 
 ?>
--- mainpage.php
+++ mainpage.php
@@ -13,51 +13,14 @@
     <?php
         require __DIR__ . '/functions.php';
 
-    //    create_personel_data();
-    //    create_taxbands();
+        echo "<pre>";
+        print_r(create_personel_data());
+        echo "</pre>";
     
-       $personeldatapre = array();
-       $personeldata = array();
-       $json = file_get_contents("./employees-final.json");
-       $personeldatapre = json_decode($json,true);
-   
-       
-       foreach($personeldatapre as $person){
-           $elementrename =  $person["id"]."_".$person["firstname"]."_".$person["lastname"];
-           $personeldata[$elementrename] = $person;
-           $elementrename=""; 
-       } //this adds the ID, firstname and last name to the parent array element, rather than normal.
-   
-       $taxdatapre = array();
-       $taxdata = array();
-       $json = file_get_contents("./tax-tables.json");
-       $taxdatapre = json_decode($json,true);
-   
-   
-       foreach($taxdatapre as $tax){
-            $elementrename =  "tax_band_".$tax["id"];
-            $taxdata[$elementrename] = $tax;
-            $elementrename=""; 
-       } //this creates an array from tax rate JSON, renaming the parent element to the tax band, and removing the name element
-   
+      
 
-       foreach($personeldata as $person){
-           $salarytocheck = $person["salary"];
-           if($salarytocheck <= $taxdata["tax_band_1"]["maxsalary"]){ // 10k
-            array_push($person, array("tax_band"=>"tax_band_1"));
-           }elseif($salarytocheck <= $taxdata["tax_band_2"]["maxsalary"]){
-            array_push($person, array("tax_band"=>"tax_band_2"));
-           }elseif($salarytocheck <= $taxdata["tax_band_3"]["maxsalary"]){
-            array_push($person, array("tax_band"=>"tax_band_3"));
-           }elseif($salarytocheck > $taxdata["tax_band_4"]["minsalary"]){
-            array_push($person, array("tax_band"=>"tax_band_4"));
-       }
-    }
-
-echo "<pre>";
-print_r($personeldata);
-echo "</pre>";
-
+    
+        
 
     ?>
 
