685f2bf98bea82a1b10eb908973df3da6ddee0dd
Jack <jack.sargeant22@gmail.com>
2021-11-01 22:51:41 +0000

    need to sort webpage

--- person.php
+++ person.php
@@ -1,15 +1,56 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Main Page</title>
+    </head>
+    <body>
 
-$person = $_GET["person"];
+    
+      
 
+       
+        <nav class='navbar navbar-light bg-warning justify-content-center h2 mb-0'>
+        <span class='navbar-brand mb-0'>Jack's PHP Project</span>
+        </nav>
+        
+        <div class="container-fluid vh-100">
 
-if (isset($_SESSION[$person])) {
-    echo "<pre>";
-    print_r($_SESSION[$person]);
-    echo "</pre>";
-}
+            <div class="d-flex flex-row h-50">
 
+                <div class="col-4 d-flex ">
+                <button type="button" class="btn btn-dark">Dark</button>
+                <a href='https://placeholder.com'><img src='https://via.placeholder.com/300' class="">
+                </div>
+                <div class="col-4">
+                
+                </div>
+                <div class="col-4">
+                <a href='https://placeholder.com'><img src='https://via.placeholder.com/100'>
+                </div>
+            </div>
 
+            <div class="d-flex flex-row h-50">
 
-?>
+            </div>
+
+        </div>
+
+        
+        
+
+        <?php
+
+        require __DIR__ . '/functions.php';
+
+        ?>
+
+            </tbody>   
+        </table>
+        
+    </body>
+</html>
